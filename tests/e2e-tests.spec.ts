502c9744310f332e8e578374e040e8b433c1eb5d
Nazar Tanin <nazar.tanin@phenixrts.com>
2022-11-09 15:22:30 +0200

    update name of e2e test

--- tests/e2e-tests.spec.ts
+++ tests/e2e-tests.spec.ts
@@ -1,16 +1,19 @@
 import { test, expect } from '@playwright/test';
 
-test('homepage has title and links to intro page', async ({ page }) => {
-  await page.goto('https://qa.awarasleep.com/');
-  await expect(page).toHaveTitle('Awara');
-  await expect(page.url()).toContain('qa.awarasleep.com');
+test.describe('e2e tests', () => {
+  test('Should add product to cart and verify it is there', async ({ page }) => {
+    await page.goto('https://qa.awarasleep.com/');
+    await expect(page).toHaveTitle('Awara');
+    await expect(page.url()).toContain('qa.awarasleep.com');
 
-  await page.getByTestId('hero_shop_mattress').click();
-  await expect(page.url()).toContain('qa.awarasleep.com/mattress');
+    await page.getByTestId('hero_shop_mattress').click();
+    await expect(page.url()).toContain('qa.awarasleep.com/mattress');
 
-  await page.getByTestId('addtocart_btn').click();
-  await expect(page).toHaveTitle('Awara - Express Checkout');
-  await expect(page.url()).toContain('qa.awarasleep.com/checkout/shipping');
-  const addedToCartItem = await page.getByTestId('cart_items_area').getByTestId('awara-latex-hybrid-mattress');
-  await expect(addedToCartItem).toBeVisible();
+    await page.getByTestId('addtocart_btn').click();
+    await expect(page).toHaveTitle('Awara - Express Checkout');
+    await expect(page.url()).toContain('qa.awarasleep.com/checkout/shipping');
+
+    const addedToCartItem = await page.getByTestId('cart_items_area').getByTestId('awara-latex-hybrid-mattress');
+    await expect(addedToCartItem).toBeVisible();
+  });
 });
